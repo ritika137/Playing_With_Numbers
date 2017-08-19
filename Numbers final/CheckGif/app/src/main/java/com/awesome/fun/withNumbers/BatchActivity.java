89d7bf541c1bad12c595921557f6c8015014a86e
ritika137 <ritikabedhara13@gmail.com>
2017-08-20 03:27:26 +0530

    final

---
+++ Numbers final/CheckGif/app/src/main/java/com/awesome/fun/withNumbers/BatchActivity.java
@@ -0,0 +1,93 @@
+package com.awesome.fun.withNumbers;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import java.util.StringTokenizer;
+
+public class BatchActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPref;
+    int [] imagesArrayList=new int[50];
+    int totalSize;
+    ScrollView scrollView;
+    GridLayout gridLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_batch);
+
+        scrollView=(ScrollView)findViewById(R.id.ScrollView);
+        gridLayout=(GridLayout)findViewById(R.id.GridLayout);
+        //gridView=(GridView)findViewById(R.id.GridView);
+
+
+
+//        Display display = getWindowManager().getDefaultDisplay();
+//        LinearLayout layout= new LinearLayout();
+//        int width=display.getWidth();
+//        int height=display.getHeight();
+//        LinearLayout.LayoutParams parms = new LinearLayout.LayoutParams(width,height);
+//        layout.setLayoutParams(parms);
+
+
+
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        totalSize=Main2Activity.totalSize;
+        //Log.i("totalsize",Integer.toString(totalSize));
+        display();
+    }
+
+    public void display(){
+
+        //fill values into imagesArrayList
+        String savedString = sharedPref.getString("string", "");
+        Log.i("value of string",savedString);
+        StringTokenizer st = new StringTokenizer(savedString, ",");
+        for (int i = 0; i < 50; i++) {
+            if(st.hasMoreTokens()) {
+                imagesArrayList[i] = Integer.parseInt(st.nextToken());
+                Log.i("Placing in batch ",Integer.toString(imagesArrayList[i]) );
+            }
+            else{
+                break;
+            }
+        }
+
+
+        //insert badges
+        for(int i=0;i<totalSize;i++){
+            ImageView imageView=new ImageView(getApplicationContext());
+            if(imagesArrayList[i]==0){
+                continue;
+            }
+            String uri = "@drawable/image" + imagesArrayList[i];
+            Log.i("screen",uri);
+            int imageResource = getResources().getIdentifier(uri, null, getPackageName());
+//            Drawable res = getResources().getDrawable(imageResource);
+//            imageView.setImageDrawable(res);
+//            scrollView.addView(imageView);
+            LinearLayout.LayoutParams imParams =
+                    new LinearLayout.LayoutParams(250, 250);
+            ImageView imSex = new ImageView(getApplicationContext());
+            imSex.setImageResource(imageResource);
+            //LinearLayout ll=(LinearLayout) findViewById(R.id.LinearLayout);
+            gridLayout.addView(imSex,imParams);
+
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ Numbers final/CheckGif/app/src/main/java/com/awesome/fun/withNumbers/MainActivity.java
@@ -0,0 +1,35 @@
+package com.awesome.fun.withNumbers;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+    SharedPreferences sharedPref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor=sharedPref.edit();
+        editor.clear();
+        editor.commit();
+        setContentView(R.layout.activity_main);
+        Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+        startActivity(intent);
+    }
+
+}
+
+
